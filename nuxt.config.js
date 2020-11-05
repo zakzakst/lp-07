a41ad37f041a360b01e5cbc64beee7d88890ea8b
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-11-05 23:07:03 +0900

    production時にconsoleを削除する

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,6 +69,13 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console: process.env.NODE_ENV === "production"
+        }
+      }
+    }
   },
 
   // 動的ルーティングの追加
