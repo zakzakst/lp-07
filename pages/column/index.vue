a344c1f8509868ab4858452456424fd557219f90
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-11-09 06:43:34 +0900

    記事一覧ページの作成

--- pages/column/index.vue
+++ pages/column/index.vue
@@ -1,19 +1,40 @@
 <template>
   <div>
-    <button
-      v-for="category in categories"
-      :key="category.id"
-      @click="setCategory(category.id)"
-    >{{ category.label }}</button>
-    <ul>
-      <li v-for="item in filteredColumnItems" :key="item.id">
-      <!-- <li v-for="item in columnItems" :key="item.id"> -->
-        <nuxt-link :to="`/column/${item.id}`">
-          {{ item.date }}：{{ item.listText }}
-        </nuxt-link>
-      </li>
-    </ul>
-    <button v-if="addBtnIsActive" @click="addItems">add</button>
+    <picture class="kv">
+      <!-- <source type="image/webp" :srcset="`${mainImg}.webp`">
+      <img :src="mainImg"> -->
+      <img src="https://fakeimg.pl/1400x200/888/000/?text=MainImage">
+    </picture>
+    <div class="container mt-6">
+      <div class="mb-12 text-center">
+        <h1 class="mb-4">{{ title }}</h1>
+        <div class="btn-group">
+          <button
+            v-for="category in categories"
+            :key="category.id"
+            @click="setCategory(category.id)"
+            class="btn btn-dark"
+            :class="{'active': category.id === categoryCurrent}"
+          >{{ category.label }}</button>
+        </div>
+      </div>
+      <transition-group name="list" mode="out-in" tag="ul" class="list">
+        <li v-for="item in filteredColumnItems" :key="item.id">
+          <nuxt-link :to="`/column/${item.id}`" class="list-item">
+            <span class="list-item__date">{{ item.date | dateFilter }}</span>
+            <span>{{ item.listText }}</span>
+          </nuxt-link>
+        </li>
+      </transition-group>
+      <div class="text-center">
+        <button
+          v-if="addBtnIsActive"
+          @click="addItems"
+          type="button"
+          class="btn btn-outline-dark"
+        >もっと見る</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,7 +55,7 @@ export default {
       title: 'コラム一覧',
       description: 'コラム一覧の概要',
       keywords: 'コラム一覧のキーワード1,コラム一覧のキーワード2',
-      category: 'all',
+      categoryCurrent: 'all',
       categories: [
         {
           id: 'all',
@@ -58,11 +79,11 @@ export default {
   methods: {
     setCategory(category) {
       // 現在のカテゴリーと同じ場合は処理を止める
-      if (this.category === category) return;
+      if (this.categoryCurrent === category) return;
       // 表示数を初期化
       this.currentItemsNum = this.startItemsNum;
       // カテゴリーを変更
-      this.category = category;
+      this.categoryCurrent = category;
     },
     addItems() {
       this.currentItemsNum += this.addItemsNum;
@@ -72,11 +93,11 @@ export default {
     filteredColumnItems() {
       let result;
       // カテゴリーでフィルター
-      if (!this.columnItems.length || this.category === 'all') {
+      if (!this.columnItems.length || this.categoryCurrent === 'all') {
         result = this.columnItems;
       } else {
         result = this.columnItems.filter(column => {
-          return column.categories.indexOf(this.category) !== -1;
+          return column.categories.indexOf(this.categoryCurrent) !== -1;
         });
       }
       // 表示数の反映
@@ -111,6 +132,12 @@ export default {
   mixins: [
     mixinMeta,
   ],
+  filters: {
+    dateFilter(val) {
+      const date = new Date(val)
+      return date.toLocaleDateString();
+    },
+  },
   head() {
     return this.getMeta();
     // headの内容を追加する場合
@@ -122,4 +149,61 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.kv {
+  position: relative;
+  display: block;
+  height: 200px;
+  overflow: hidden;
+  & > img {
+    position: absolute;
+    display: block;
+    height: 200px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+.btn{
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+}
+.list {
+  list-style: none;
+  margin: 0 0 16px;
+  padding: 0;
+  & > li {
+    border-bottom: 1px dotted #333;
+    &:first-child {
+      border-top: 1px dotted #333;
+    }
+  }
+}
+.list-item {
+  display: flex;
+  padding: 8px;
+  color: $color-text-default;
+  text-decoration: none;
+}
+.list-item__date {
+  width: 100px;
+  flex-shrink: 0;
+}
+
+.list-enter-active {
+  transition: opacity .5s, transform .5s;
+}
+.list-leave-active {
+  opacity: 0;
+  position: absolute;
+}
+.list-move:not(.list-leave-active) {
+  transition: opacity .5s, transform .5s;
+}
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+}
+</style>
